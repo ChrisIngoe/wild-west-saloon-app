5a0566191aa6d5d562b9cd7c438882f716a2f08f
Chris Ingoe <chris.software.engineer@gmail.com>
2020-10-27 22:26:29 +0000

    create starter app code base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import bgImage from './saloon.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundImage:`url(${bgImage})` }}>
+    </div>
+  );
+}
+
+export default App;
