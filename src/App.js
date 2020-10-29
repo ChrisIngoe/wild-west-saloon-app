374c9888fb6ba1eaeee7ba3e1cebdf200c2b46a2
Chris Ingoe <chris.software.engineer@gmail.com>
2020-10-29 22:49:41 +0000

    Ahem, fix typo...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import bgImage from './saloon.jpg';
 import './App.css';
 import SingleDrinkButton from './components/drinks/singleDrinkButton';
